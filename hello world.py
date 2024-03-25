7429c1ff3165351fc6f4b0b3dbc823d4f9d19386
tashanita <153968245+tashanita@users.noreply.github.com>
2024-03-25 19:43:45 +0700

    Add files via upload

---
+++ hello world.py
@@ -0,0 +1,10 @@
+# print("hello world")
+# print("hello my name is tasha")
+# print("i live in depok city")
+# print("i'm 10 years old")
+# print(5+5)
+# print(8*4)
+#print(3*4/2+8)
+print("""Leonardo da Vinci (1452-1519) was an Italian artist, inventor, and scientist who is 
+      widely considered to be one of the greatest minds of the Renaissance era.
+       Born in the town of Vinci in Tuscany, he was the illegitimate son of a wealthy notary and a peasant woman.""")
