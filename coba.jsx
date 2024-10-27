30e311352a97830c2840f2fa28a72621ef862cf2
tashanita <153968245+tashanita@users.noreply.github.com>
2024-10-27 10:07:13 +0700

    Create coba.jsx

---
+++ coba.jsx
@@ -0,0 +1,14 @@
+<div>
+  <title>my first JSX</title>
+  <style
+    dangerouslySetInnerHTML={{
+      __html: "\n        a{\n            padding:left;\n        }\n    ",
+    }}
+  />
+  <header>
+    <a href="#">home</a>
+    <a href="#">learn coding</a>
+    <a href="#">fun fact</a>
+    <a href="#">Q&A</a>
+  </header>
+</div>;
